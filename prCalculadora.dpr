2ace502a1d5a8a5d253e7e5b5d995500b20fc0a4
Jamison Duarte da Silva <jamisonduarte23@gmail.com>
2018-12-25 17:16:52 -0200

    IMPORTAR UNIT DE OPERAÇÕES MATEMATICAS DO PROJETO DE CALCULADORA DO VCL-FORMS

--- prCalculadora.dpr
+++ prCalculadora.dpr
@@ -3,12 +3,13 @@ program prCalculadora;
 uses
   System.StartUpCopy,
   FMX.Forms,
-  Unit1 in 'Unit1.pas' {Form1};
+  UFrCalculadora in 'UFrCalculadora.pas' {frCalculadora},
+  UOperacoesMat in 'UOperacoesMat.pas';
 
 {$R *.res}
 
 begin
   Application.Initialize;
-  Application.CreateForm(TForm1, Form1);
+  Application.CreateForm(TfrCalculadora, frCalculadora);
   Application.Run;
 end.
